7647057e897d0b6b1750adec18cf9faa645a51c1
Midstackdev <midstackdev@gmail.com>
2019-11-12 11:45:59 +0000

    Displaying areas and persisting it in session

---
+++ app/Http/Controllers/User/AreaController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Area;
+use Illuminate\Http\Request;
+
+class AreaController extends Controller
+{
+    public function store(Area $area)
+    {
+    	session()->put('area', $area->slug);
+
+    	return redirect()->back();
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,29 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                @foreach($areas as $country)
+                    <div class="col-md-12">
+                        <h3><a href="{{ route('user.area.store', $country) }}">{{ $country->name }}</a></h3>
+                        <hr>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                        <div class="row">
+                            @foreach($country->children as $state)
+                                <div class="col-md-4">
+                                    <h4><a href="{{ route('user.area.store', $state) }}">{{ $state->name }}</a></h4>
+                                    <hr>
+                                    
+                                    @foreach($state->children as $city)
+                                        <h5><a href="{{ route('user.area.store', $city) }}">{{ $city->name }}</a></h5>
+                                    @endforeach
+
+                                </div>
+                            @endforeach
                         </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
